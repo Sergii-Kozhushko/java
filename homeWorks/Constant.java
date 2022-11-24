7fca84960bd84a8f1794a93953bbb8e47abffe59
Sergii Kozhushko <117940475+Sergii-Kozhushko@users.noreply.github.com>
2022-11-24 17:33:11 +0100

    Add files via upload

---
+++ homeWorks/Constant.java
@@ -0,0 +1,14 @@
+/*
+   Programmed by Sergii Kozhushko 2022
+   Date of creation: 24-Nov-2022 
+*/
+
+package game.hero;/*
+Programmed by Sergii Kozhushko
+*/
+
+public class Constant {
+    static int ELFPOWER = 90;
+    static int ELFCHARISMA = 80;
+    static int ELFMAGIC = 80;
+}
---
+++ homeWorks/Elf.java
@@ -0,0 +1,14 @@
+package game.hero;/*
+   Programmed by Sergii Kozhushko 2022
+   Date of creation: 24-Nov-2022 
+*/
+
+public class Elf{
+    int id;
+    String name;
+    int age;
+    int power;//сила
+    int accuracy;
+    int stamina;
+}
+
